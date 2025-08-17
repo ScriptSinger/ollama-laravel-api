c0f3f4ca9c68fcf314310448f2a28b4b11145f6e
ScriptSinger <ufamasters102@gmail.com>
2025-08-17 23:46:41 +0500

    feat(config): configure environment for SPA authentication with Sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Http\Controllers\Api\AIController;
+use App\Http\Controllers\Api\ChatSessionController;
+use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\SettingController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('users', UserController::class);
+
+
+
+
+
+Route::middleware(['auth:sanctum'])->apiResource('chat-sessions', ChatSessionController::class);
+
+
+
+Route::apiResource('messages', MessageController::class);
+// ->only(['store', 'show']);
+
+
+Route::get('settings', [SettingController::class, 'index']);
+Route::put('settings', [SettingController::class, 'update']);
+
+
+Route::post('ai/generate', [AIController::class, 'generate']);
